7584c001068b3eac9867f1bcb82697cbbf453053
akihanari <akihanari@gmail.com>
2020-04-04 01:49:19 +0900

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
-        'USER': 'name',
+        'USER': 'akihanari',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
