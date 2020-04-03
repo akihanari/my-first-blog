484b2eaf02b7d254452bfa81f408fc4cce4746fb
akihanari <akihanari@gmail.com>
2020-04-04 03:10:24 +0900

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,5 +139,8 @@ LOGIN_REDIRECT_URL = '/'
 #     },
 # }
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
